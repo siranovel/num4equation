1c660055bd4405d1d5c2c3a85999627f35d16925
siranovel <siranovel@gmail.com>
2023-05-21 12:25:41 +0900

    Add files via upload

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -1,68 +1,59 @@
 require 'num4equ'
 
 class Num4EquTest
+    def initialize
+        @f1 = proc {|x| 
+          next x * x - 2
+        }
+        @f1x = proc {|x| 
+          next x * x - 4
+        }
+        @f2 = proc {|x| x * x - x + 1}
+    end
     def newtonMethodTest
-        f1 = proc {|x| x * x - 2}
-        f2 = proc {|x| x * x - x + 1}
-        df1 = proc {|x| 2 * x}
-        df2 = proc {|x| 2 * x - 1}
-        begin
-            p "newtonMethodTest in"
-            x = Num4EquLib.newtonMethod(
-              -1.8, f1, df1
-            )
-            print "3 x:", x
-            puts
-            x = Num4EquLib.newtonMethod(
-             -3, f2, df2
-            )
-            print "-3 x:", x
-            puts
-        rescue => e
-            p e.class
-            p e.message
-            p e.backtrace
-        end
+        p "newtonMethodTest in"
+        x = Num4EquLib.newtonMethod(-1.8, @f1)
+        print "-1.8 x:", x
+        puts
+        x = Num4EquLib.newtonMethod(-3, @f2)
+        print "-3 x:", x
+        puts
+    rescue => e
+        p e.class
+        p e.message
+        p e.backtrace
     end
     def bisectionMethodTest
-        f1 = proc {|x| x * x - 2}
-        f2 = proc {|x| x * x - x + 1}
-        begin
-            p "bisectionMethodTest in"
-            x = Num4EquLib.bisectionMethod(-3, -1, f1)
-            print "-3, -1 x:", x
-            puts
-            #x = Num4EquLib.bisectionMethod(2, 3, f1)
-            #print "2,   3 x:", x
-            #puts
-            x = Num4EquLib.bisectionMethod(-3, 4, f2)
-            print "-3,   4 x:", x
-            puts
-        rescue => e
-            p e.class
-            p e.message
-            p e.backtrace
-        end
+        p "bisectionMethodTest in"
+        x = Num4EquLib.bisectionMethod(-3, -1, @f1)
+        print "-3, -1 x:", x
+        puts
+        #x = Num4EquLib.bisectionMethod(2, 3, @f1)
+        #print "2,   3 x:", x
+        #puts
+        x = Num4EquLib.bisectionMethod(-3, 4, @f2)
+        print "-3,   4 x:", x
+        puts
+    rescue => e
+        p e.class
+        p e.message
+        p e.backtrace
     end
     def secantMethodTest
-        f1 = proc {|x| x * x - 2}
-        f2 = proc {|x| x * x - x + 1}
-        begin
-            p "secantMethodTest in"
-            x = Num4EquLib.secantMethod(-1, 3, f1)
-            print "-1, 3 x:", x
-            puts
-            x = Num4EquLib.secantMethod(2, 3, f1)
-            print "2,   3 x:", x
-            puts
-            x = Num4EquLib.secantMethod(-3, 4, f2)
-            print "-3,   4 x:", x
-            puts
-        rescue => e
-            p e.class
-            p e.message
-            p e.backtrace
-        end
+        p "secantMethodTest in"
+        x = Num4EquLib.secantMethod(-1, 3, @f1)
+        print "-1, 3 x:", x
+        puts
+        x = Num4EquLib.secantMethod(2, 3, @f1)
+        print "2,   3 x:", x
+        puts
+        x = Num4EquLib.secantMethod(-3, 4, @f2)
+        print "-3,   4 x:", x
+        puts
+    rescue => e
+        p e.class
+        p e.message
+        p e.backtrace
     end
 end
 tst = Num4EquTest.new
